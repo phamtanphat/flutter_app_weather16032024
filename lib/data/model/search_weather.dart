2b04b84b8905fbc8370d210e7bc90e06f51ff20e
phatdroid94 <phatdroid94@gmail.com>
2024-10-17 19:08:21 +0700

    model search weather

---
+++ lib/data/model/search_weather.dart
@@ -0,0 +1,15 @@
+class SearchWeather {
+  String name = "";
+  num temp = 0;
+  num minTemp = 0;
+  num maxTemp = 0;
+  String status = "";
+
+  SearchWeather({
+    required this.name,
+    required this.temp,
+    required this.minTemp,
+    required this.maxTemp,
+    required this.status
+  });
+}
